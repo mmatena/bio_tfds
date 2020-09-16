c4fb594acbe0ff06332586e8c4f5af98162ac38b
Michael Matena <michael.matena@gmail.com>
2020-09-16 17:41:36 -0400

    Default commit message.

--- scripts/download_and_prepare_mhcflurry.py
+++ scripts/download_and_prepare_mhcflurry.py
@@ -7,4 +7,7 @@ checksum_dir = "/nas/longleaf/home/mmatena/projects/bio_tfds/url_checksums"
 tfds.download.add_checksums_dir(checksum_dir)
 
 ds = mhcflurry.MhcBindingAffinity()
-ds.download_and_prepare(download_dir=DEFAULT_TFDS_DOWNLOAD_DIR)
+ds.download_and_prepare(
+    download_dir=DEFAULT_TFDS_DOWNLOAD_DIR,
+    download_config=tfds.download.DownloadConfig(register_checksums=True),
+)
